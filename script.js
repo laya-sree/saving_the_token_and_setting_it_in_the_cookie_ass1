6a379e25bb26f106dc47244ef2693664a401824c
laya-sree <kolathur.layasree@kalvium.community>
2025-04-08 12:39:23 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
 
 const decrypt = (token) => {
-  // return decoded payload
+  // return decoded payload using jwt.verify
+  return jwt.verify(token, process.env.JWT_SECRET);
 }
+
+const payload = { name: 'laya', id: 22};
+
+const token = encrypt(payload);
+console.log('JWT Token:', token);
+
+const decoded = decrypt(token);
+console.log('Decoded Payload:', decoded);
 
 module.exports = {
   encrypt,
-  decrypt
+  decrypt
 }
